35a351430db1e56138398b47d1059bc5c8c70537
Gaston-Pandullo <140354738+Gaston-Pandullo@users.noreply.github.com>
2025-03-02 18:58:18 -0300

    Agregado de AlignYourBodyElement

--- app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -15,7 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,9 +68,38 @@ fun SearchBar(
 
 @Composable
 fun AlignYourBodyElement(
+    @StringRes text: Int, // Al poner int se hace referencia al id del recurso.
+    @DrawableRes image: Int,
     modifier: Modifier = Modifier
 ){
+    Column(
+     horizontalAlignment = Alignment.CenterHorizontally,
+     modifier = modifier
+ ) {
+     Image(
+         painter = painterResource(image),
+         contentDescription = null,
+         contentScale = ContentScale.Crop,
+         modifier = Modifier.size(88.dp).clip(CircleShape)
+     )
+        Text(
+            text = stringResource(text),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+ }
+}
 
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AlignYourBodyPreview(){
+    BasicLayoutsCodelabTheme {
+        AlignYourBodyElement(
+            image = R.drawable.ab1_inversions,
+            text = R.string.ab1_inversions,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
